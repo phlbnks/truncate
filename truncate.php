faf9202431d273e5cb3f08b01d9fd4a7c00657c5
emirpprime <stickmanphotography@gmail.com>
2011-03-31 16:32:41 +0100

    function file

---
+++ truncate.php
@@ -0,0 +1,59 @@
+<?php
+
+function truncate($type='words', $string, $length, $break='.', $pad='')
+{
+	switch($type) {
+		case 'words':
+			switch($break) {
+				// Split at the next end of a sentence PAST the $length. 
+				case '.':
+					$output = strtok($string, " \n");
+					while(--$length > 0) $output .= " " . strtok(" \n");
+					if (substr($output, -1) !== '.') $output .= " " . strtok(".\n") . ".";
+					return $output;
+				break;
+				// Split at the next end of a word PAST the $length. 
+				case ' ':
+					$output = strtok($string, " \n");
+					while(--$length > 0) $output .= " " . strtok(" \n");
+					if($output != $string) $output .= $pad;
+					return $output;
+				break;
+			}
+		break;
+		case 'chars':
+			switch($break) {
+				// Split at the next end of a sentence PAST the $length. 
+				case '.':
+					if(strlen($string) <= $length) return $string;
+					if(false !== ($breakpoint = strpos($string, $break, $length))) {
+						if($breakpoint < strlen($string) - 1) {
+							$string = substr($string, 0, $breakpoint) . ". " . $pad;
+						}
+					} 
+					return $string;
+				break;
+				// Split at the next end of a word PAST the $length. 
+				case ' ':
+					if(strlen($string) <= $length) return $string;
+					if(false !== ($breakpoint = strpos($string, $break, $length))) {
+						if($breakpoint < strlen($string) - 1) {
+							$string = substr($string, 0, $breakpoint) . $pad;
+						}
+					} 
+					return $string;
+				break;
+				// Split at the next character PAST the $length. 
+				case '*':
+					if(strlen($string) <= $length) return $string;
+					else {
+						$string = substr($string, 0, $length) . $pad;
+					}
+					return $string;
+				break;
+			}
+		break;
+	}
+}
+
+?>
